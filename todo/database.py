c42c1724cf7186402699d152e6678d683c266b33
JW5123 <Jay2002714@gmail.com>
2025-07-02 21:27:19 +0800

    feat: Code the add, remove, list operation functionality

--- todo/database.py
+++ todo/database.py
@@ -1,6 +1,9 @@
 import configparser
+import json
 from pathlib import Path
 from todo import DB_WRITE_ERROR, SUCCESS
+from typing import Any, Dict, List, NamedTuple
+from todo import DB_READ_ERROR, DB_WRITE_ERROR, JSON_ERROR, SUCCESS
 
 DEFAULT_DB_FILE_PATH = Path.home().joinpath("." + Path.home().stem + "_todo.json")
 
@@ -15,3 +18,30 @@ def init_database(db_path: Path) -> int:
         return SUCCESS
     except OSError:
         return DB_WRITE_ERROR
+    
+class DBResponse(NamedTuple):
+    todo_list: List[Dict[str, Any]]
+    error: int
+
+class Databasehandler:
+    def __init__(self, db_path: Path) -> None:
+        self._db_path = db_path
+    
+    def read_todos(self) -> DBResponse:
+        try:
+            with self._db_path.open("r") as db:
+                try:
+                    return DBResponse(json.load(db), SUCCESS)
+                except json.JSONDecodeError:
+                    return DBResponse([], JSON_ERROR)
+        except OSError:
+            return DBResponse([], DB_READ_ERROR)
+    
+    def write_todos(self, todo_list: List[Dict[str, Any]]) -> int:
+        try:
+            with self._db_path.open("w") as db:
+                json.dump(todo_list, db, indent=4)
+            return DBResponse(todo_list, SUCCESS)
+        except OSError:
+            return DBResponse(todo_list, DB_WRITE_ERROR)
+        
